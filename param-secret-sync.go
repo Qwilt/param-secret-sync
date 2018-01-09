44755b48c6564dd97dfd3690db89e430171355a2
Nir Dothan <nird@qwilt.com>
2018-01-09 18:04:38 +0200

    add vendor, complete build

--- param-secret-sync.go
+++ param-secret-sync.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
-	"github.com/nirdothan/param-secret-sync/version"
+	"github.com/nirdothan/param-secret-sync/pkg/version"
 	apicorev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
